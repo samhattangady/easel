e72c5ee227558a736e901aab42cf0bffb2db2f45
Samarth Hattangady <samhattangady@gmail.com>
2020-10-15 23:30:24 +0530

    feat: add vulkan dependencies

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 mkdir build
 pushd build
-"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.26.28801\bin\Hostx64\x64\cl.exe"  /Fe:easel.exe /Zi /I "C:\Users\user\libraries\SDL2-2.0.12\include" ..\src\main.c /link "C:\Users\user\libraries\SDL2-2.0.12\lib\x64\SDL2main.lib" "C:\Users\user\libraries\SDL2-2.0.12\lib\x64\SDL2.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64\shell32.lib" /SUBSYSTEM:CONSOLE
+"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.26.28801\bin\Hostx64\x64\cl.exe"  /Fe:easel.exe /Zi /I "C:\Users\user\libraries\SDL2-2.0.12\include" /I "C:\VulkanSDK\1.2.154.1\Include" ..\src\main.c /link "C:\Users\user\libraries\SDL2-2.0.12\lib\x64\SDL2main.lib" "C:\Users\user\libraries\SDL2-2.0.12\lib\x64\SDL2.lib" "C:\VulkanSDK\1.2.154.1\Lib\vulkan-1.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.18362.0\um\x64\shell32.lib" /SUBSYSTEM:CONSOLE
 popd
--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
-#include<stdio.h>
+#include "SDL.h"
+#include "SDL_vulkan.h"
+#include <vulkan/vulkan.h>
+#include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Hello Easel.\n");
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+    int init_success = SDL_Init(SDL_INIT_VIDEO);
+    if (init_success != 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not initalise SDL: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    SDL_Window* window;
+    window = SDL_CreateWindow("Easel", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                              1024, 768, SDL_WINDOW_VULKAN);
+    if (window == NULL) {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window %s\n", SDL_GetError());
+        SDL_Quit();
+        return -2;
+    }
+
+    int extensions = 0;
+    vkEnumerateInstanceExtensionProperties(NULL, &extensions, NULL);
+    SDL_Log("Vulkan seems to support %i extensions.\n", extensions);
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
