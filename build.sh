00ecabd643ade822db70796be6433b48f87b915f
Samarth Hattangady <samhattangady@gmail.com>
2020-10-19 23:01:03 +0530

    feat: update linux build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-mkdir build && pushd build && time gcc -O1 -o easel ../src/main.c -I/usr/include/SDL2 -lSDL2 -lvulkan  && popd && ./build/easel && rm -rf build
+mkdir build && pushd build && time gcc -O1 -o easel ../src/main.c ../src/es_painter.c ../src/es_warehouse.c -I/usr/include/SDL2 -lSDL2 -lvulkan  && popd && ./build/easel && rm -rf build
